34ca228277d71d06440ccda49ffa77933e7148d8
Juyeong Maing <mjy9088@naver.com>
2024-03-11 22:51:04 +0900

    remove unnecessary operations

--- include/mb_core_type.h
+++ include/mb_core_type.h
@@ -6,7 +6,7 @@
 /*   By: jmaing <jmaing@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 23:34:31 by jmaing            #+#    #+#             */
-/*   Updated: 2024/03/10 23:35:10 by jmaing           ###   ########.fr       */
+/*   Updated: 2024/03/11 22:50:21 by jmaing           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,85 +17,50 @@
 
 # include "mb_rt_type.h"
 
-typedef bool								t_err;
+typedef bool				t_err;
 
-typedef struct s_mb_real					t_mb_real;
+typedef struct s_mb_real	t_mb_real;
 
-typedef void								(*t_mb_real_v_delete)(
-												t_mb_real *self);
+typedef void				(*t_mb_real_v_delete)(
+								t_mb_real *self);
 
-typedef t_err								(*t_mb_real_v_clone)(
-												t_mb_real *self,
-												t_mb_real **out);
+typedef t_err				(*t_mb_real_v_clone)(
+								t_mb_real *self,
+								t_mb_real **out);
 
-typedef t_err								(*t_mb_real_v_assign)(
-												t_mb_real **lhs,
-												t_mb_real *rhs);
+typedef t_err				(*t_mb_real_v_assign)(
+								t_mb_real **lhs,
+								t_mb_real *rhs);
 
-typedef t_err								(*t_mb_real_v_binary)(
-												t_mb_real *lhs,
-												t_mb_real *rhs,
-												t_mb_real **out);
+typedef t_err				(*t_mb_real_v_binary)(
+								t_mb_real *lhs,
+								t_mb_real *rhs,
+								t_mb_real **out);
 
-typedef t_err								(*t_mb_real_v_check)(
-												t_mb_real *self,
-												bool *out);
+typedef t_err				(*t_mb_real_v_check)(
+								t_mb_real *self,
+								bool *out);
 
-typedef t_err								(*t_mb_real_v_relation)(
-												t_mb_real *lhs,
-												t_mb_real *rhs,
-												bool *out);
+typedef t_err				(*t_mb_real_v_to_float)(
+								t_mb_real *self,
+								float *out);
 
-typedef t_err								(*t_mb_real_v_to_string)(
-												t_mb_real *self,
-												char **out);
-
-typedef t_err								(*t_mb_real_v_from_string)(
-												char *string,
-												t_mb_real **out);
-
-typedef t_err								(*t_mb_real_v_to_float)(
-												t_mb_real *self,
-												float *out);
-
-typedef t_err								(*t_mb_real_v_from_float)(
-												float value,
-												t_mb_real **out);
-
-typedef t_err								(*t_mb_real_v_to_buffer)(
-												t_mb_real *self,
-												t_mb_rt_buffer *out);
-
-typedef t_err								(*t_mb_real_v_from_buffer)(
-												t_mb_rt_buffer buffer,
-												t_mb_real **out);
+typedef t_err				(*t_mb_real_v_from_float)(
+								float value,
+								t_mb_real **out);
 
 typedef struct s_mb_real_type
 {
-	t_mb_real_v_delete		delete;
-	t_mb_real_v_clone		clone;
-	t_mb_real_v_binary		add;
-	t_mb_real_v_binary		subtract;
-	t_mb_real_v_binary		multiply;
-	t_mb_real_v_binary		divide;
-	t_mb_real_v_assign		assign;
-	t_mb_real_v_assign		assign_add;
-	t_mb_real_v_assign		assign_subtract;
-	t_mb_real_v_assign		assign_multiply;
-	t_mb_real_v_assign		assign_divide;
-	t_mb_real_v_check		is_nan;
-	t_mb_real_v_check		is_infinity;
-	t_mb_real_v_relation	eq;
-	t_mb_real_v_relation	greater;
-	t_mb_real_v_relation	lesser;
-	t_mb_real_v_to_string	to_string;
-	t_mb_real_v_from_string	from_string;
-	t_mb_real_v_to_float	to_float;
-	t_mb_real_v_from_float	from_float;
-	t_mb_real_v_to_buffer	to_buffer;
-	t_mb_real_v_from_buffer	from_buffer;
+	t_mb_real_v_delete	delete;
+	t_mb_real_v_clone	clone;
+	t_mb_real_v_binary	add;
+	t_mb_real_v_binary	multiply;
+	t_mb_real_v_assign	assign;
+	t_mb_real_v_assign	assign_add;
+	t_mb_real_v_assign	assign_multiply;
+	t_mb_real_v_check	is_greater_than_or_equal_to_two;
 }	t_mb_real_type;
 
-typedef struct s_mb							t_mb;
+typedef struct s_mb			t_mb;
 
 #endif
